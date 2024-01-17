60612a29f183a5029a132d03a288f6c85a1ccdf2
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-17 14:47:26 +0100

    Commit 8

--- src/main/java/giorgiomigliaccio/U5W2L2/controllers/BlogController.java
+++ src/main/java/giorgiomigliaccio/U5W2L2/controllers/BlogController.java
@@ -3,6 +3,8 @@ package giorgiomigliaccio.U5W2L2.controllers;
 
 import giorgiomigliaccio.U5W2L2.entities.Blog;
 import giorgiomigliaccio.U5W2L2.services.BlogService;
+//import epicode.u5d7hw.exceptions.NotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,42 +12,41 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/blog")
-public class BlogController {
-
+@RequestMapping("/blogs")
+public class BlogsController {
     @Autowired
-    private BlogService blogService;
+    BlogsService blogsService;
 
-    // 1. GET http://localhost:3001/blog
-    @GetMapping
-    public List<Blog> getBlog() {
-        return blogService.getBlog();
+    // 1. - POST http://localhost:3001/blogs (+ req.body)
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED) // <-- 201
+    public Blogpost saveBlog(@RequestBody Blogpost body) {
+        return blogsService.save(body);
     }
 
-    // 	2. POST http://localhost:3001/users (+body)
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED) // Status Code 201
-    public Blog saveBlog(@RequestBody Blog body) {
-        return blogService.save(body);
+    // 2. - GET http://localhost:3001/blogs
+    @GetMapping("")
+    public List<Blogpost> getBlogs() {
+        return blogsService.getBlogs();
     }
 
-    // 	3. GET http://localhost:3001/users/:id
-    @GetMapping("/{id}")
-    public Blog findById(@PathVariable int id) {
-        return blogService.findById(id);
+    // 3. - GET http://localhost:3001/blogs/{id}
+    @GetMapping("/{blogId}")
+    public Blogpost findById(@PathVariable int blogId) {
+        return blogsService.findById(blogId);
     }
 
-
-    // 	4. PUT http://localhost:3001/users/:id (+body)
-    @PutMapping("/{id}")
-    public Blog findByAndUpdate(@PathVariable int id, @RequestBody Blog body) {
-        return this.blogService.findByIdAndUpdate(id, body);
+    // 4. - PUT http://localhost:3001/blogs/{id} (+ req.body)
+    @PutMapping("/{blogId}")
+    public Blogpost findAndUpdate(@PathVariable int blogId, @RequestBody Blogpost body) {
+        return blogsService.findByIdAndUpdate(blogId, body);
     }
 
-    // 	5. DELETE http://localhost:3001/users/:id
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) // Status Code 204 (No content)
-    public void findByIdAndDelete(@PathVariable int id) {
-        this.blogService.findByIdAndDelete(id);
+    // 5. - DELETE http://localhost:3001/blogs/{id
+    @DeleteMapping("/{blogId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // <-- 204 NO CONTENT
+    public void findAndDelete(@PathVariable int blogId) {
+        blogsService.findByIdAndDelete(blogId);
     }
 }
+
