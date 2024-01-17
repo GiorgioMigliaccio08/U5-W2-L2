7f389962944cb163712b168763441a2b9547e113
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-17 14:44:14 +0100

    Commit 7

--- src/main/java/giorgiomigliaccio/U5W2L2/controllers/AutoreController.java
+++ src/main/java/giorgiomigliaccio/U5W2L2/controllers/AutoreController.java
@@ -3,49 +3,57 @@ package giorgiomigliaccio.U5W2L2.controllers;
 import giorgiomigliaccio.U5W2L2.entities.Autore;
 import giorgiomigliaccio.U5W2L2.services.AutoreService;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/autore")
 public class AutoreController {
-
     @Autowired
-    private AutoreService autoreService;
+    AutoreServiceService authorsService;
 
-    // 1. GET http://localhost:3001/blog
-    @GetMapping
-    public List<Autore> getBlog() {
-        return autoreService.getBlog();
+    // 1. - POST http://localhost:3001/authors (+ req.body)
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED) // <-- 201
+    public Author saveAuthor(@RequestBody Author body) throws Exception {
+        System.out.println(body);
+        return authorsService.save(body);
     }
 
-    // 	2. POST http://localhost:3001/users (+body)
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED) // Status Code 201
-    public Autore saveBlog(@RequestBody Autore body) {
-        return autoreService.save(body);
+    // 2. - GET http://localhost:3001/authors
+    @GetMapping("")
+    public List<Author> getAuthors() {
+        return authorsService.getAuthors();
     }
 
-    // 	3. GET http://localhost:3001/users/:id
-    @GetMapping("/{id}")
-    public Autore findById(@PathVariable int id) {
-        return autoreService.findById(id);
+    // 3. - GET http://localhost:3001/authors/{id}
+    @GetMapping("/{authorId}")
+    public Author findById(@PathVariable int authorId) throws Exception {
+        return authorsService.findById(authorId);
     }
 
-
-    // 	4. PUT http://localhost:3001/users/:id (+body)
-    @PutMapping("/{id}")
-    public Autore findByAndUpdate(@PathVariable int id, @RequestBody Autore body) {
-        return this.autoreService.findByIdAndUpdate(id, autore);
+    // 4. - PUT http://localhost:3001/authors/{id} (+ req.body)
+    @PutMapping("/{authorId}")
+    public Author findAndUpdate(@PathVariable int authorId, @RequestBody Author body) throws Exception {
+        return authorsService.findByIdAndUpdate(authorId, body);
     }
 
-    // 	5. DELETE http://localhost:3001/users/:id
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) // Status Code 204 (No content)
-    public void findByIdAndDelete(@PathVariable int id) {
-        this.autoreService.findByIdAndDelete(id);
+    // 5. - DELETE http://localhost:3001/authors/{id}
+    @DeleteMapping("/{authorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // <-- 204 NO CONTENT
+    public void findAndDelete(@PathVariable int authorId) {
+        authorsService.findByIdAndDelete(authorId);
     }
 }
+
