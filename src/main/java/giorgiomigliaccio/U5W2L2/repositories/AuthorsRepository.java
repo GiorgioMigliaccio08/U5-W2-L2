2a2d2cbd96d016c265955d1559de0511096b76d9
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-18 16:39:44 +0100

    Commit 15

--- src/main/java/giorgiomigliaccio/U5W2L2/repositories/AuthorsRepository.java
+++ src/main/java/giorgiomigliaccio/U5W2L2/repositories/AuthorsRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 public class AuthorsRepository extends JpaRepository<Autore, Integer> {
+    extends JpaRepository<Author, Integer>
 }
