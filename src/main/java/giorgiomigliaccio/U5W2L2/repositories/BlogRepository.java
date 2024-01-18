568ecd2b4cfc2fe55256bb460c792067303a52c0
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-01-18 16:41:40 +0100

    Commit 17

--- src/main/java/giorgiomigliaccio/U5W2L2/repositories/BlogRepository.java
+++ src/main/java/giorgiomigliaccio/U5W2L2/repositories/BlogRepository.java
@@ -1,9 +1,13 @@
 package giorgiomigliaccio.U5W2L2.repositories;
 
 
+
 import epicode.u5d8hw.entities.Author;
 import epicode.u5d8hw.entities.Blogpost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 public class BlogRepository {
 }
